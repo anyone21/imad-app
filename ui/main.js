a1a46c30d7147b3bb5940b6c1c24875082b118bb
annoying_code <32765126+anyone21@users.noreply.github.com>
2018-02-21 22:46:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('My application');
 // change the text of the main-text div
 var element = document.getElementById('main-text'); 
 element.innerHTML = 'New- data has been loaded';
+
+//Move the image
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+  img.style.marginLeft = '100px';     
+};
