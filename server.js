e2a498b45ba586e698b3368a1557e42865610f8e
annoying_code <32765126+anyone21@users.noreply.github.com>
2018-02-22 03:09:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0; 
+app.get('/counter',function(req,res)
+{
+  counter = counter + 1;
+  res.send(counter.toString()); 
+});
+
 app.get('/:articleName',function(req,res){
     // articleName == article-one
     // articles[articleName] == {} content object for article one
@@ -95,12 +102,7 @@ app.get('/article-three',function(req,res){
   res.send('Article three is requested and will be served');
 });
 
-var counter = 0; 
-app.get('/counter',function(req,res)
-{
-  counter = counter+1;
-  res.send(counter.toString()); 
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
