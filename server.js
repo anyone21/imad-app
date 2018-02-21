209d41bacf955ef0d19d340bee239fb162ec41e8
annoying_code <32765126+anyone21@users.noreply.github.com>
2018-02-21 19:19:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+  title:'Article - One | ujjwal bansal',
+  heading:'Article One',
+  date:'FEB 21,2018',
+  content:`
+            <p>
+        My name isujjjwal bansal . why is this code not working! Damn! What should I do now? 
+        Hope my code doesn't break on live, take care Pratik. It's old code, this function is
+        used at 1583 places, making change in it is like awakening a sleeping monster! 
+        </p>
+        <P>
+        We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly
+        once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+        </p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate=`
+        <html>
+          <head>
+            <title> 
+                ${title}
+            </title>
+            <meta name="viewport" content="width-device-width, initial-scale-1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+          </head>
+            <body>
+              <div class="container">
+                  <div>
+                      <o href="/">Home</o>
+                  </div>
+              </div>
+              <hr/>
+              <h3> ${heading} </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+             </div>
+                
+            </body>
+        </html>
+    `;
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res)
 {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res)
