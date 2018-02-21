ed408174030cb99ffc69e8eb57ed4954c0761c81
annoying_code <32765126+anyone21@users.noreply.github.com>
2018-02-22 03:32:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('My application');
 // change the text of the main-text div
 var element = document.getElementById('main-text'); 
@@ -14,4 +15,17 @@ function moveRight()
 img.onclick = function()
 {
   var interval = setInterval(moveRight,50);    
+};
+*/
+// var counter code
+var button = document.getElementById("counter");
+var counter =0;
+button.onclick = function()
+{
+  // make a response to the counter endpoint
+  //capture the responsea nad store it in the variabe
+  //render the variable in the correct span
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
